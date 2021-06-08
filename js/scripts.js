c94ec0f1cd006e8822cd748a4b27ea6272a5cbbd
Clara Heinz <clarakheinz@gmail.com>
2021-06-08 19:36:32 -0400

    add button

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// find element on web page
+const actionButton = document.getElementById("action-btn");
+// perform action on click
+actionButton.addEventListener("click", performActionOnClick)
+
+// function definition
+function performActionOnClick() {
+    console.log("TEST");
+}
