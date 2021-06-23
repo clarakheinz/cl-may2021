031ec107adcb6dbcf6065f224b8c3f1c4ea41c8a
Clara Heinz <clarakheinz@gmail.com>
2021-06-23 11:50:24 -0400

    Add button functionality with loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,33 @@
 // find element on web page
 const actionButton = document.getElementById("action-btn");
 // perform action on click
-actionButton.addEventListener("click", performActionOnClick)
+actionButton.addEventListener("click", performOnClick)
+
+
+let dataCollection = [];
+
+const listItems = document.getElementById('list-items');
+
+
+function performOnClick() {
+    const added = prompt('What do you want to add?');
+    dataCollection.push(added);
+
+    addItem(dataCollection);
+}
 
 // function definition
-function performActionOnClick() {
-    console.log("TEST");
+function addItem(dataCollection) {
+    while(listItems.firstChild) {
+        listItems.removeChild(listItems.firstChild);
+    }
+
+    
+    for (let i=0; i < dataCollection.length; i++) {
+        const listItem = document.createElement('LI');
+        const textValue = document.createTextNode(dataCollection[i]);
+    
+        listItem.appendChild(textValue);
+        listItems.appendChild(listItem);
+    }
 }
